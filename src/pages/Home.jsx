005255dfcd2caf546417a4636b0bed078f6b562b
ShankenduK <shankendu@gmail.com>
2025-02-21 23:57:04 +0530

    Fixed responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Navbar />
       <div className="p-6 w-full h-[80vh] flex flex-col justify-center items-center pt-28">
         <div className="w-fit space-y-3">
-          <h1 className="font-archivo text-7xl sm:text-8xl md:text-9xl dark:[text-shadow:_5px_5px_0px_#F1C40F] [text-shadow:_5px_5px_0px_#FF5733]">
+          <h1 className="font-archivo text-6xl sm:text-8xl md:text-9xl dark:[text-shadow:_5px_5px_0px_#F1C40F] [text-shadow:_5px_5px_0px_#FF5733]">
             QUIZART
           </h1>
           <div className="w-full flex items-center font-medium justify-around font-ibm text-[#333333] dark:text-[#bbbbbb] text-sm sm:text-base">
@@ -19,9 +19,9 @@ const Home = () => {
           </div>
         </div>
         
-        <div className="mt-20 space-x-5">
-        <input className=" border-2 border-black dark:border-white dark:text-white text-black font-medium px-5 py-3 rounded-full font-ibm text-2xl [box-shadow:5px_5px_0px_#000000] dark:[box-shadow:5px_5px_0px_#FFFFFF] outline-none" onChange={(e) => setUsername(e.target.value)} value={username} type="text" placeholder="Enter your name" />
-          <button disabled={!username.trim()} onClick={() => navigate("/rule")} className="bg-[#FF5733] hover:bg-[#C70039] border-2 border-black dark:border-white dark:bg-[#F1C40F] dark:hover:bg-[#FFA500] dark:text-white text-black font-medium px-5 py-3 rounded-full font-ibm text-2xl [box-shadow:5px_5px_0px_#000000] dark:[box-shadow:5px_5px_0px_#FFFFFF] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(82_82_82)]">
+        <div className="mt-20 space-x-5 flex flex-col sm:flex-row justify-center items-center">
+        <input className=" border-2 border-black dark:border-white dark:text-white text-black font-medium px-5 py-3 rounded-full font-ibm text-lg sm:text-2xl [box-shadow:5px_5px_0px_#000000] dark:[box-shadow:5px_5px_0px_#FFFFFF] outline-none mb-3 sm:mb-0" onChange={(e) => setUsername(e.target.value)} value={username} type="text" placeholder="Enter your name" />
+          <button disabled={!username.trim()} onClick={() => navigate("/rule")} className="bg-[#FF5733] hover:bg-[#C70039] border-2 border-black dark:border-white dark:bg-[#F1C40F] dark:hover:bg-[#FFA500] dark:text-white text-black font-medium px-5 py-3 rounded-full font-ibm text-lg sm:text-2xl [box-shadow:5px_5px_0px_#000000] dark:[box-shadow:5px_5px_0px_#FFFFFF] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(82_82_82)]">
             START QUIZ
           </button>
         </div>
